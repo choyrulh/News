45361135ca83bf3923433e7a3c52508f3e7c620c
choyrulh <choirul.humam@students.amikom.ac.id>
2024-06-12 00:39:09 +0700

    update

--- api/app.js
+++ api/app.js
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
 // Enable CORS for all routes
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
